7f5d75baf3798a2ce5c4f2f10c612c258468e6c4
arib7701 <amandineribot01@gmail.com>
2019-08-31 21:05:29 +0200

    clean useless variables

--- sncf-api-producer/src/main/java/ribot/amandine/kafka/app/model/DisruptionApiResponse.java
+++ sncf-api-producer/src/main/java/ribot/amandine/kafka/app/model/DisruptionApiResponse.java
@@ -6,22 +6,10 @@ import java.util.List;
 
 public class DisruptionApiResponse {
 
-    private Integer totalResult;
-    private Integer startPage;
     private List<Disruption> disruptionList;
 
-    public DisruptionApiResponse(Integer totalResult, Integer startPage, List<Disruption> disruptionList) {
-        this.totalResult = totalResult;
-        this.startPage = startPage;
+    public DisruptionApiResponse(List<Disruption> disruptionList) {
         this.disruptionList = disruptionList;
-    }
-
-    public Integer getTotalResult() {
-        return totalResult;
-    }
-
-    public Integer getStartPage() {
-        return startPage;
     }
 
     public List<Disruption> getDisruptionList() {
