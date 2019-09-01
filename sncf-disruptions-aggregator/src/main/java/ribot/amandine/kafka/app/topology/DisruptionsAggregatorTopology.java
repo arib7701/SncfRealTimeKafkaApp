b0666a8088d97fd0d4a92a597b0f8cf40343104b
arib7701 <amandineribot01@gmail.com>
2019-09-01 21:04:32 +0200

    create basic topology that listen to Disruption stream

--- sncf-disruptions-aggregator/src/main/java/ribot/amandine/kafka/app/topology/DisruptionsAggregatorTopology.java
+++ sncf-disruptions-aggregator/src/main/java/ribot/amandine/kafka/app/topology/DisruptionsAggregatorTopology.java
@@ -3,168 +3,72 @@ package ribot.amandine.kafka.app.topology;
 import com.typesafe.config.ConfigFactory;
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.state.KeyValueStore;
-import ribot.amandine.kafka.app.*;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
+import ribot.amandine.kafka.app.Disruption;
+import ribot.amandine.kafka.app.KeyDisruption;
+import ribot.amandine.kafka.app.aggregators.ComplexAggregators;
+import ribot.amandine.kafka.app.aggregators.SimpleAggregators;
 import ribot.amandine.kafka.app.configuration.AppConfig;
+import ribot.amandine.kafka.app.transformers.DisruptionTransformer;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Properties;
 
 public class DisruptionsAggregatorTopology {
 
     private final AppConfig appConfig;
+    private final SpecificAvroSerde<Disruption> disruptionSpecificAvroSerde = new SpecificAvroSerde<>();
+    private final SpecificAvroSerde<KeyDisruption> keyDisruptionSpecificAvroSerde = new SpecificAvroSerde<>();
+    private SimpleAggregators simpleAggregators;
+    private ComplexAggregators complexAggregators;
 
     public DisruptionsAggregatorTopology(Properties properties) {
 
         appConfig = new AppConfig(ConfigFactory.load());
+        simpleAggregators = new SimpleAggregators();
+        complexAggregators = new ComplexAggregators();
         KafkaStreams streams = createTopology(properties);
         streams.start();
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
+    private StoreBuilder<KeyValueStore<KeyDisruption, Disruption>> createStateStore(){
+
+        return Stores
+                .keyValueStoreBuilder((Stores.persistentKeyValueStore("disruptions-store")),
+                        keyDisruptionSpecificAvroSerde,
+                        disruptionSpecificAvroSerde);
+    }
+
     private KafkaStreams createTopology(Properties properties) {
 
-        SpecificAvroSerde<Disruption> disruptionSpecificAvroSerde = new SpecificAvroSerde<>();
-        SpecificAvroSerde<StopStatistic> stopStatisticSpecificAvroSerde = new SpecificAvroSerde<>();
-        SpecificAvroSerde<TrainStatistic> trainStatisticSpecificAvroSerde = new SpecificAvroSerde<>();
-        SpecificAvroSerde<CauseStatistic> causeStatisticSpecificAvroSerde = new SpecificAvroSerde<>();
-        Serdes.StringSerde stringSerde = new Serdes.StringSerde();
-
         disruptionSpecificAvroSerde.configure(Collections.singletonMap(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, appConfig.getSchemaRegistryUrl()), false);
-        stopStatisticSpecificAvroSerde.configure(Collections.singletonMap(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, appConfig.getSchemaRegistryUrl()), false);
-        trainStatisticSpecificAvroSerde.configure(Collections.singletonMap(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, appConfig.getSchemaRegistryUrl()), false);
-        causeStatisticSpecificAvroSerde.configure(Collections.singletonMap(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, appConfig.getSchemaRegistryUrl()), false);
+        keyDisruptionSpecificAvroSerde.configure(Collections.singletonMap(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, appConfig.getSchemaRegistryUrl()), false);
 
         StreamsBuilder builder = new StreamsBuilder();
+        builder.addStateStore(this.createStateStore());
 
-        KStream<String, Disruption> disruptionKStream = builder.stream(appConfig.getDisruptionTopicName(), Consumed.with(Serdes.String(), disruptionSpecificAvroSerde));
+        KStream<KeyDisruption, Disruption> disruptionKStream = builder
+                .stream(appConfig.getDisruptionTopicName(), Consumed.with(keyDisruptionSpecificAvroSerde, disruptionSpecificAvroSerde))
+                .transform(DisruptionTransformer::new, "disruptions-store")
+                .peek(((key, value) -> System.out.println(value.toString())))
+                .filter((key, value) -> key != null && value != null);
 
+        simpleAggregators.aggregateStopFromStream(disruptionKStream);
+        simpleAggregators.aggregateTrainFromStream(disruptionKStream);
+        simpleAggregators.aggregateCauseFromStream(disruptionKStream);
 
-        // GET STATISTIC OF DISRUPTIONS PER STOP ID
-
-        KStream<String, Stop> disruptionStreamPerStopName = disruptionKStream
-                .flatMap((key, value) -> {
-                    List<KeyValue<String, Stop>> results = new ArrayList<>();
-
-                    for (int i = 0; i < value.getStops().size(); i++) {
-                        results.add(new KeyValue<>(value.getStops().get(i).getId().toString(), value.getStops().get(i)));
-                    }
-
-                    return results;
-                });
-
-
-        KTable<String, StopStatistic> stopStatisticsKTable = disruptionStreamPerStopName
-                .groupByKey()
-                .aggregate(
-                        this::initStopStatistic,
-                        this::aggregateStopStatistic,
-                        Materialized.<String, StopStatistic, KeyValueStore<Bytes, byte[]>>as("stop-statistic-table-store")
-                                .withKeySerde(stringSerde)
-                                .withValueSerde(stopStatisticSpecificAvroSerde));
-
-        stopStatisticsKTable
-                .toStream()
-                .to(appConfig.getStopStatsTopicName(), Produced.with(stringSerde, stopStatisticSpecificAvroSerde));
-
-
-        // GET STATISTIC OF DISRUPTIONS PER TRAIN ID
-
-        KStream<String, Train> disruptionStreamPerTrainName = disruptionKStream
-                .map((key, value) -> KeyValue.pair(value.getTrain().getId().toString(), value.getTrain()));
-
-
-        KTable<String, TrainStatistic> trainStatisticKTable = disruptionStreamPerTrainName
-                .groupByKey()
-                .aggregate(
-                        this::initTrainStatistic,
-                        this::aggregateTrainStatistic,
-                        Materialized.<String, TrainStatistic, KeyValueStore<Bytes, byte[]>>as("train-statistic-table-store")
-                                .withKeySerde(stringSerde)
-                                .withValueSerde(trainStatisticSpecificAvroSerde));
-
-        trainStatisticKTable
-                .toStream()
-                .to(appConfig.getTrainStatsTopicName(), Produced.with(stringSerde, trainStatisticSpecificAvroSerde));
-
-
-        // GET STATISTIC OF DISRUPTIONS PER CAUSE
-
-        KStream<String, Disruption> disruptionStreamPerCause = disruptionKStream
-                .filter((key, value) -> value.getMessage() != null)
-                .map((key, value) -> KeyValue.pair(value.getMessage().toString(), value));
-
-
-        KTable<String, CauseStatistic> causeStatisticsKTable = disruptionStreamPerCause
-                .groupByKey()
-                .aggregate(
-                        this::initCauseStatistic,
-                        this::aggregateCauseStatistic,
-                        Materialized.<String, CauseStatistic, KeyValueStore<Bytes, byte[]>>as("cause-statistic-table-store")
-                                .withKeySerde(stringSerde)
-                                .withValueSerde(causeStatisticSpecificAvroSerde));
-
-        causeStatisticsKTable
-                .toStream()
-                .to(appConfig.getCauseStatsTopicName(), Produced.with(stringSerde, causeStatisticSpecificAvroSerde));
+        complexAggregators.aggregateByCauseAndStop(disruptionKStream);
+        complexAggregators.aggregateByTrainAndStop(disruptionKStream);
+        complexAggregators.aggregateByTrainAndCause(disruptionKStream);
+        complexAggregators.aggregateByStopAndTrainAndCause(disruptionKStream);
 
         return new KafkaStreams(builder.build(), properties);
-    }
-
-    private StopStatistic initStopStatistic() {
-
-        return StopStatistic.newBuilder()
-                .setNumberDisruption(0)
-                .build();
-    }
-
-    private StopStatistic aggregateStopStatistic(String stopId, Stop stop, StopStatistic currentStats) {
-
-        return StopStatistic.newBuilder(currentStats)
-                .setId(stopId)
-                .setName(stop.getName())
-                .setLatitude(stop.getLatitude())
-                .setLongitude(stop.getLongitude())
-                .setNumberDisruption(currentStats.getNumberDisruption()+1)
-                .build();
-    }
-
-    private TrainStatistic initTrainStatistic() {
-
-        return TrainStatistic.newBuilder()
-                .setNumberDisruption(0)
-                .build();
-    }
-
-    private TrainStatistic aggregateTrainStatistic(String trainId, Train train, TrainStatistic currentStats) {
-
-        return TrainStatistic.newBuilder(currentStats)
-                .setId(trainId)
-                .setName(train.getName())
-                .setNumberDisruption(currentStats.getNumberDisruption()+1)
-                .build();
-    }
-
-    private CauseStatistic initCauseStatistic() {
-
-        return CauseStatistic.newBuilder()
-                .setNumberDisruption(0)
-                .build();
-    }
-
-    private CauseStatistic aggregateCauseStatistic(String text, Disruption disruption, CauseStatistic currentStats) {
-
-        return CauseStatistic.newBuilder(currentStats)
-                .setText(text)
-                .setNumberDisruption(currentStats.getNumberDisruption()+1)
-                .build();
     }
 }
