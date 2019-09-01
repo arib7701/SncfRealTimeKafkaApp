8a3e2c0b684d85ed7479eec61f011153098c4bee
arib7701 <amandineribot01@gmail.com>
2019-09-01 21:04:01 +0200

    set up config for topics of aggregations

--- sncf-disruptions-aggregator/src/main/java/ribot/amandine/kafka/app/configuration/AppConfig.java
+++ sncf-disruptions-aggregator/src/main/java/ribot/amandine/kafka/app/configuration/AppConfig.java
@@ -7,10 +7,21 @@ public class AppConfig {
     private final String bootstrapServers;
     private final String schemaRegistryUrl;
     private final String disruptionTopicName;
-    private final String stopStatsTopicName;
-    private final String trainStatsTopicName;
-    private final String causeStatsTopicName;
     private final String applicationId;
+
+    // FOR SIMPLE STATS
+    private final String stopStatsTopicName;
+    private final String stopStatsPerDayTopicName;
+    private final String trainStatsTopicName;
+    private final String trainStatsPerDayTopicName;
+    private final String causeStatsTopicName;
+    private final String causeStatsPerDayTopicName;
+
+    // FOR COMPLEX STATS
+    private final String stopCauseStatsTopicName;
+    private final String stopTrainStatsTopicName;
+    private final String trainCauseStatsTopicName;
+    private final String stopTrainCauseStatsTopicName;
 
     public AppConfig(Config config) {
         this.bootstrapServers = config.getString("kafka.bootstrap.servers");
@@ -18,8 +29,15 @@ public class AppConfig {
         this.disruptionTopicName = config.getString("kafka.disruption.topic.name");
         this.applicationId = config.getString("kafka.streams.application.id");
         this.stopStatsTopicName = config.getString("kafka.stop.stats.topic.name");
+        this.stopStatsPerDayTopicName = config.getString("kafka.stop.stats.per.day.topic.name");
         this.trainStatsTopicName = config.getString("kafka.train.stats.topic.name");
+        this.trainStatsPerDayTopicName = config.getString("kafka.train.stats.per.day.topic.name");
         this.causeStatsTopicName = config.getString("kafka.cause.stats.topic.name");
+        this.causeStatsPerDayTopicName = config.getString("kafka.cause.stats.per.day.topic.name");
+        this.stopCauseStatsTopicName = config.getString("kafka.stop.cause.stats.topic.name");
+        this.stopTrainStatsTopicName = config.getString("kafka.stop.train.stats.topic.name");
+        this.trainCauseStatsTopicName = config.getString("kafka.train.cause.stats.topic.name");
+        this.stopTrainCauseStatsTopicName = config.getString("kafka.stop.train.cause.stats.topic.name");
     }
 
     public String getBootstrapServers() {
@@ -42,7 +60,22 @@ public class AppConfig {
         return stopStatsTopicName;
     }
 
+    public String getStopStatsPerDayTopicName() { return stopStatsPerDayTopicName; }
+
     public String getTrainStatsTopicName() { return trainStatsTopicName; }
 
+    public String getTrainStatsPerDayTopicName() { return trainStatsPerDayTopicName; }
+
     public String getCauseStatsTopicName() { return causeStatsTopicName; }
+
+    public String getCauseStatsPerDayTopicName() { return causeStatsPerDayTopicName; }
+
+    public String getStopCauseStatsTopicName() { return stopCauseStatsTopicName; }
+
+    public String getStopTrainStatsTopicName() { return stopTrainStatsTopicName; }
+
+    public String getTrainCauseStatsTopicName() { return trainCauseStatsTopicName; }
+
+    public String getStopTrainCauseStatsTopicName() { return stopTrainCauseStatsTopicName; }
+
 }
